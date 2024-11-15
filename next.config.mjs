fb95f1adbae3e6df588342c6618f1c45e1f0cd92
Aymane <motadaymane@gmail.com>
2024-11-15 18:55:58 +0100

    Changing next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+
 const nextConfig = {
-    typescript: {
-        // !! WARNING !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARNING !!
-        ignoreBuildErrors: true,
-      },
+  typescript: {
+    // !! WARNING !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARNING !!
+    ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve('./'), // This maps '@/' to the project root
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
