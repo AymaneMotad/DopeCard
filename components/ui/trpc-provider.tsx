86285479acd92d53c209e3c8ba6d47bea7cac4ab
Aymane <motadaymane@gmail.com>
2024-12-20 17:12:35 +0100

    BaseUrl batch

--- components/ui/trpc-provider.tsx
+++ components/ui/trpc-provider.tsx
@@ -10,10 +10,13 @@ import { trpc } from "../../server/client";
 export function TRPCProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({}));
   const [trpcClient] = useState(
+    const baseUrl = (process.env.NEXT_PUBLIC_APP_URL as string) || "http://localhost:3000";
+    const fullUrl = `${baseUrl}/api/trpc`;
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: `${process.env.NEXT_PUBLIC_APP_URL}/api/trpc`,
+          //url: `${process.env.NEXT_PUBLIC_APP_URL}/api/trpc`,
+          url: fullUrl
         }),
       ],
     })
