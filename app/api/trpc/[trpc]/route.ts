da79feb00600eb68897aca81d191aa8af53e20d3
Aymane <motadaymane@gmail.com>
2024-12-27 18:05:17 +0100

    Finally fixed trpc header

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,18 +1,29 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-
 import { appRouter } from "@/server";
 
 const handler = async (req: Request) => {
-//  console.log("Incoming request URL:", req.url); // Log the request URL
+  console.log('Incoming trpc request', req.url);
 
-  // Call the fetchRequestHandler and return the result to handle the request properly
-  return fetchRequestHandler({
+    // check if the request is for our mutation, and it's a POST request
+    const isUsersCreate = req.url.includes("/api/trpc/users.create") && req.method === "POST";
+
+  const response = await fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
     createContext: () => ({}),
   });
+
+   if(isUsersCreate) {
+        console.log('setting content type header')
+        response.headers.set('Content-Type', 'application/vnd.apple.pkpass');
+        response.headers.set(
+            'Content-Disposition',
+            `attachment; filename=ADSK2D-pass.pkpass`,
+        );
+        console.log('Response headers', response.headers);
+    }
+    return response
 };
+
 export { handler as GET, handler as POST };
-
-
