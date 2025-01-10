25a8844ed74c4c9ffff33de121929aae540eeffc
Aymane <motadaymane@gmail.com>
2025-01-10 17:11:09 +0100

    Fix env varibale issue

--- app/utils/pass-generation/pass-generation.ts
+++ app/utils/pass-generation/pass-generation.ts
@@ -6,21 +6,22 @@ import { google } from 'googleapis';
 import jwt from 'jsonwebtoken';
 
 
-// Your service account JSON (replace with the actual JSON)
 const serviceAccount = {
     type: process.env.SERVICE_ACCOUNT_TYPE,
     project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
     private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-    private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
-    auth_uri: process.env.AUTH_URI,
-    token_uri: process.env.TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-    universe_domain: process.env.UNIVERSE_DOMAIN
+    private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+    client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
+    auth_uri: process.env.SERVICE_ACCOUNT_AUTH_URI,
+    token_uri: process.env.SERVICE_ACCOUNT_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.SERVICE_ACCOUNT_UNIVERSE_DOMAIN
 };
 
+
+//console.log('service account is', serviceAccount)
 
 
 export async function generatePass(userId: string, stampCount: number = 0) {
